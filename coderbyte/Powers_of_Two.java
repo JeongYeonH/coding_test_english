2c3cd199dab6af1c4faadc265dff88654ba43687
Jeong Yeon Hwang <kukuro1995@naver.com>
2025-07-04 12:46:27 +0900

    처음 영어로

--- coderbyte/Powers_of_Two.java
+++ coderbyte/Powers_of_Two.java
@@ -1,5 +1,20 @@
 package coderbyte;
 
+import java.util.Scanner;
+
 public class Powers_of_Two {
-    
+    public static boolean PowersOfTwo(String num) {
+        boolean isPow = false;
+        int rest = Integer.parseInt(num);
+        while(rest%2 == 0 && rest >2){
+            rest = rest/2;
+        }
+        if(rest == 2) isPow = true;
+        return isPow;
+    }
+
+    public static void main (String[] args) {  
+        Scanner s = new Scanner(System.in);
+        System.out.print(PowersOfTwo(s.nextLine())); 
+    }
 }
